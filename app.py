07f6741eeb9f4a5d776345a9d9517e67e14bb9f1
Nanciee722 <2781340243@qq.com>
2026-05-09 16:23:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ def text2audio(story_text):
 # Main part
 st.set_page_config(page_title="Kids Story App", page_icon="https://img.icons8.com/?size=100&id=jfFu3i8zJXfN&format=png&color=000000")
 st.markdown(
-    '<img src="https://img.icons8.com/?size=100&id=LlgB5a8aAr0G&format=png&color=000000" width="50"> '
+    '<img src="https://img.icons8.com/?size=100&id=LlgB5a8aAr0G&format=png&color=000000" width="60"> '
     '<span style="font-size:28px; font-weight:bold;">Image Storytelling for Kids</span>',
     unsafe_allow_html=True
 )
